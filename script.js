a9e64dc741ad6a0d7e8135c734862b9f8c3a04eb
Sergio Vergara <sergiovergara123@gmail.com>
2024-03-12 14:48:21 -0500

    function setPlayerCards completed to show myFavoriteFootballTeam objects in DOM

--- script.js
+++ script.js
@@ -170,6 +170,28 @@ const myFavoriteFootballTeam = {
     },
   ],
 };
+
 Object.freeze(myFavoriteFootballTeam);
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr
+    .map(
+      ({ name, position, number, isCaptain, nickname }) =>
+      `
+        <div class="player-card">
+          <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+      `
+    )
+    .join("");
+};
